3b9aa86d71aed8a7446814eedc4691432beee5ee
Isaac Mosiah <isaacmosiah@alu.ufc.br>
2023-09-14 13:31:21 -0300

    bugged favicon

--- overfitting/src/app/layout.tsx
+++ overfitting/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="shortcut icon" href="/favicon.ico" sizes='any'/>
+        <link rel="shortcut icon" href="../../public/favicon.ico" sizes='any'/>
       </head>
       <body className='body'>
         <Navbar/>
